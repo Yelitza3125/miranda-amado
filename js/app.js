410cab328c41c1051c0b8c5ec59dec77e95e40ec
AraceliGS <aragutarras@gmail.com>
2018-04-11 17:48:07 -0500

    añadiendo filtrado de los sindicatos

--- js/app.js
+++ js/app.js
@@ -11,8 +11,37 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 var info = database.ref('convenios');
+var sindicato = document.getElementById('checkbox1');
+var industry = document.getElementById('checkbox2');
+var section = document.getElementById('section');
 /*Función para obtener los datos*/
 info.on('value', function(datos) {
+  // debugger;
   data = datos.val();
-  console.log(data);
-    });
+  //  Evento change al checkbox sindicato
+  sindicato.addEventListener('change', function() {
+    if(sindicato.checked === true) {
+      // var section = document.createElement('section');
+      // section.id='section';
+      // console.log(data);
+      data.forEach(function(el) {
+        // console.log(el.Sindicato);
+        var content = el.Sindicato;
+        var div = document.createElement('div');
+        div.innerHTML = content;
+        div.className = 'filter-item';
+        section.appendChild(div);
+        document.body.appendChild(section);
+        // console.log(section.children);
+      });
+    }
+    if(sindicato.checked === false) {
+      // debugger;
+      console.log('Desactivastes el checkbox, por lo que no se verá nada');
+      var children = document.getElementsByClassName('filter-item');
+      section.remove();
+    }
+  });
+
+  
+});
