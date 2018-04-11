f73d8b2cb0b0af58ac4535bd6c8a85cb1aa58e4e
JenniferCarmen <jennifercarmens@gmail.com>
2018-04-11 18:25:04 -0500

    subiendo cambios

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Initialize Firebase
-var config = {
+let config = {
   apiKey: "AIzaSyAsb9kQKq_1uiBKsChewFZp3ELY_MG4wxc",
   authDomain: "miranda-y-amado.firebaseapp.com",
   databaseURL: "https://miranda-y-amado.firebaseio.com",
@@ -9,10 +9,76 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database();
-var info = database.ref('convenios');
+let database = firebase.database();
+let info = database.ref('convenios');
 /*Función para obtener los datos*/
-info.on('value', function(datos) {
+info.on('value', function (datos) {
   data = datos.val();
-  console.log(data);
+  function listEmpresas() {
+    let Type = '';
+    let ObjectTypes = [];
+    for (i = 0; i < Object.keys(data).length; i++) {
+      Type = data[i]['Empresa'];
+      Type = data[i]['Empresa'];
+      id = data[i]['N°'] - 1,
+        ObjectTypes.push({
+          id: id,
+          name: Type
+        });
+    }
+    console.log(ObjectTypes);
+    return ObjectTypes;
+  }
+
+  $('.token-input').tokenInput(
+    listEmpresas(), {
+      theme: 'bootstrap',
+      preventDuplicates: true,
+      hintText: 'Escribe un termino de busqueda',
+      noResultsText: 'No se encntraron Resultados',
+      searchingText: 'Buscando...',
+      tokenLimit: 1
     });
+  function filterCompany(company) {
+    let resultCompany = [];
+    info.on('value', function (datos) {
+      data = datos.val();
+      data.forEach(element => {
+        if (element.Empresa == company)
+          resultCompany.push(element);
+      });
+      localStorage.setItem('result', JSON.stringify(resultCompany))
+      console.log(resultCompany);
+
+      resultCompany.forEach(element => {
+        let fecha = element.Suscripción;
+        let fechames = fecha.slice(0, 10);
+        $('#convenio').append(`
+        
+          <div class="card-body">
+          <h5 class="card-title">${element.Empresa} </h5>
+          <p class="card-text"> ${fechames} </p>
+        </div>
+        `);
+
+
+        $('.card-body').click(function () {
+          window.open(`${element.URL}`, '_blank');
+        });
+      })
+    });
+
+  }
+  $('#buscar').click(function () {
+    let company = $('.token-input').tokenInput('get')[0]['name'];
+    let inputvalue = $('.token-input').tokenInput('get');
+    if (inputvalue.length === 0) {
+      alert('ingrese un valor');
+    } else {
+      $('#convenio').empty();
+      filterCompany(company);
+
+    }
+  });
+
+});
