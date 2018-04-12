2c677b6d57a3eef95bdea40352f2542b3311ef94
carlacentenor <carlacentenor@gmail.com>
2018-04-12 12:26:16 -0500

    estilos

--- js/compare.js
+++ js/compare.js
@@ -10,36 +10,36 @@ const compare = () => {
         let fechames = fecha.slice(0, 10);
 
         let template = `
-    <div class="col-md-3 col-lg-3 box">
-      <div class="card-header">${element.Empresa}
+    <div class="col-6  col-lg-3 box">
+      <div class="card-header bold-none">${element.Empresa}
       </div>   
   </div>
 `
 
         $('#compare-box').append(template);
         $('.Industria').append(`  
-        <div class="col-3 text-center">${element.Industria}</div> 
+        <div class="col-5 col-lg-3 text-center">${element.Industria}</div> 
     <br>
     `);
         $('.compare').append(`  
-        <div class="col-3 text-center">${fechames}</div> 
+        <div class="col-5  col-lg-3 text-center">${fechames}</div> 
     <br>
     `);
         $('.Bonificación').append(`  
-    <div class="col-3 text-center">${element.Bonificación}</div> 
+    <div class="col-5  col-lg-3 text-center">${element.Bonificación}</div> 
 <br>
 `);
         $('.Incremento').append(`  
-<div class="col-3 text-center">${element.Incremento}</div> 
+<div class="col-5  col-lg-3 text-center">${element.Incremento}</div> 
 <br>
 `);
 
         $('.Sindicato').append(`  
-<div class="col-3 text-center">${element.Sindicato}</div> 
+<div class="col-5  col-lg-3 text-center">${element.Sindicato}</div> 
 <br>
 `);
         $('.Vigencia').append(`  
-<div class="col-3 text-center">${element.Vigencia}</div> 
+<div class="col-5  col-lg-3 text-center">${element.Vigencia}</div> 
 <br>
 `);
 
