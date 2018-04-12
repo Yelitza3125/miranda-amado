4b6bfe41442a080aafff3c0aee4ae9bac550f80b
AraceliGS <aragutarras@gmail.com>
2018-04-12 00:31:12 -0500

    añadiendo funcionalidad de filtrado de los convenios colectivos segun el sindicato y la industria de las empresas

--- js/app.js
+++ js/app.js
@@ -150,6 +150,7 @@ function filterVigence(date) {
 
 }
 
+
 let syndicate = document.getElementById('checkbox1');
 let industry = document.getElementById('checkbox2');
 let selectSyndicates = $('#select-syndicates');
@@ -157,28 +158,129 @@ let selectIndustries = $('#select-industries');
 info.on('value', function (datos) {
   data = datos.val();
   // Mostrar los filtros escogidos en el modal
+  let nameSelectSyndicate = false;
   syndicate.addEventListener('change', function () {
     if (syndicate.checked === true) {
+      nameSelectSyndicate = true;
       selectSyndicates.addClass('show');
       selectSyndicates.removeClass('hide');
     }
 
     if (syndicate.checked === false) {
+      nameSelectSyndicate = false;
       selectSyndicates.addClass('hide');
       selectSyndicates.removeClass('show');
     }
   });
 
+  let nameSelectIndustry = false;
+
   industry.addEventListener('change', function () {
     if (industry.checked === true) {
+      nameSelectIndustry = true;
       selectIndustries.addClass('show');
       selectIndustries.removeClass('hide');
     }
     if (industry.checked === false) {
+      nameSelectIndustry = false;
       selectIndustries.addClass('hide');
       selectIndustries.removeClass('show');
     }
   });
+
+  let syndicateSelected = '';
+  selectIndustries.change(function () {
+  optionSelected = $('select[id=select-3]').val();
+  syndicateSelected = optionSelected;
+  console.log(syndicateSelected);
+  });
+
+  let industrySelected = '';
+  selectSyndicates.change(function () {
+  optionSelected2 = $('select[id=select-2]').val();
+  industrySelected = optionSelected2;
+  console.log(industrySelected);
+  });
+
+  // Filtro por Sindicato
+
+  function filterSyndicate(syndicate) {
+    debugger;
+    let resultSyndicate = [];
+    data.forEach(element => {
+      if(element.Sindicato === syndicate) {
+        resultSyndicate.push(element);
+      }
+    });
+    localStorage.syndicate = JSON.stringify(resultSyndicate);
+  }
+  
+  // Filtro por Industria
+
+  function filterIndustry(industry) {
+    let resultIndustry = [];
+    data.forEach(el => {
+      if(el.Industria === industry) {
+        resultIndustry.push(el);
+      }
+    });
+    localStorage.industry = JSON.stringify(resultIndustry);
+  }
+  
+ let buttonFilter = $('#filter-type');
+
+ // Filtrando empresas según la industria a la que pertenecen
+
+  buttonFilter.on('click', function() {
+    $('#container-box').empty();
+    // Filtrando empresas por sindicato
+    if(nameSelectSyndicate === true) {
+      console.log('hola');
+      filterSyndicate($('select[id=select-2]').val());
+      let dataSyndicate = localStorage.syndicate;
+      let arraySyndicate = JSON.parse(dataSyndicate);
+      arraySyndicate.forEach(element => {
+        let fecha = element.Suscripción;
+        let fechames = fecha.slice(0, 10);
+        let template = `<div class="col-6 col-lg-3 box"><div class="card bg-light mb-3  " >
+        <div class="card-header">${element.Empresa}</div>
+        <div class="card-body">
+        <h5 class="card-title">${element.Sindicato}</h5>
+        <p class="card-text">${fechames}</p>
+        </div>
+        </div>
+         </div>`
+        $('#container-box').append(template);
+        $('.box').click(function () {
+          window.open(`${element.URL}`, '_blank');
+        });
+      });
+    }
+    // Filtrando empresas según la industria a la que pertenecen
+    if(nameSelectIndustry === true) {
+      console.log('hola');
+      filterIndustry($('select[id=select-3]').val());
+      let dataIndustry = localStorage.industry;
+      let arrayIndustry = JSON.parse(dataIndustry);
+      arrayIndustry.forEach(element => {
+        let fecha = element.Suscripción;
+        let fechames = fecha.slice(0, 10);
+        let template = `<div class="col-6 col-lg-3 box"><div class="card bg-light mb-3  " >
+        <div class="card-header">${element.Empresa}</div>
+        <div class="card-body">
+        <h5 class="card-title">${element.Industria}</h5>
+        <p class="card-text">${fechames}</p>
+        </div>
+        </div>
+         </div>`
+        $('#container-box').append(template);
+        $('.box').click(function () {
+          window.open(`${element.URL}`, '_blank');
+        });
+      });
+    }
+  });
+
   // Mostrar los 20 primeros
   let news = data.slice(0, 19);
   news.forEach(element => {
@@ -199,6 +301,7 @@ info.on('value', function (datos) {
   });
 
 });
+
 
 // Seleccionar tipo de filtro solo Empresa
 let nameSelectCompany = false;
@@ -257,7 +360,7 @@ checkSuscripcion.on('change', function() {
 
 $('#filter-type').on('click', function () {
   
-   $('#container-box').empty();
+  $('#container-box').empty();
   if (nameSelectCompany === true) {
     filterCompany(nameCompany);
     let dataResult = localStorage.result;
@@ -279,6 +382,4 @@ $('#filter-type').on('click', function () {
       });
     });
   }
-
-
 })
