280656960e4a2a5aaa7b3e9f1eda86cb68222bdb
AraceliGS <aragutarras@gmail.com>
2020-12-22 22:52:02 -0500

    Mostrando los 20 primeros convenios, corrigiendo errores al abrir PDF por convenio

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ let info = database.ref('convenios');
 /*Función para obtener los datos*/
 info.on('value', function (datos) {
   data = datos.val();
-  console.log(data);
+  console.log(Object.keys(data));
   function listEmpresas() {
     let Type = '';
     let ObjectTypes = [];
@@ -199,9 +199,9 @@ function filterCompany(company) {
 info.on('value', function (datos) {
   data = datos.val();
   // Mostrar los 20 primeros
-  let news = data.slice(0, 19);
+  let news = data.slice(0, 20);
   
-  news.forEach(element => {
+  news.forEach((element, index) => {
     let fecha = element.Suscripción;
     let fechames = fecha.slice(0, 10);
     let tem = `<div class="col-12 col-lg-4"><div class="container-convenio m-3">
@@ -220,7 +220,7 @@ info.on('value', function (datos) {
       <button class="btn btn-detalles btn-view" id=${element["N°"]}>
       Ver detalles
       </button>
-      <button class="btn btn-pdf text-blue-miranda"> Ver PDF
+      <button class="btn btn-pdf text-blue-miranda" data-el=${element["N°"]}> Ver PDF
       </button>
     </div>
   </div>
@@ -233,7 +233,8 @@ info.on('value', function (datos) {
 
     $('#container-box').append(tem);
     $('.btn-pdf').click(function () {
-      window.open(`${element.URL}`, '_blank');
+      if($(this).attr("data-el") == (index+1))
+        window.open(`${element.URL}`, '_blank');
     });
 
   });
